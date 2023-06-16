864e585903760725a8b02ef2b26c26e0edcec078
Jared <jaredjrsong@gmail.com>
2023-06-16 19:16:18 +1000

    added falling stars background

---
+++ app.js
@@ -0,0 +1,17 @@
+setInterval(createStars, 100);
+
+function createStars() {
+    const star = document.createElement('i');
+    star.classList.add('fa');
+    star.classList.add('fa-star');
+    star.style.left = Math.random() * window.innerWidth - 50 + 'px';
+    star.style.animationDuration = Math.random() * 3 + 2 + 's';
+    star.style.transform = `rotate(${Math.random() * 180 + 'deg'})`;
+    star.style.opacity = Math.random();
+    star.style.fontSize = Math.random() * 10 + 10 + 'px';
+    document.body.appendChild(star);
+
+    setTimeout(() => {
+        star.remove();
+    }, 4700)
+}
